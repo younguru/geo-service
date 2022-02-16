b40e3ced6106589ef494f2fb10707c145fdee330
Kirill Makarochkin <kiosun@mail.ru>
2022-02-16 16:53:56 +0300

    00

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,22 @@
-public class Test {
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
+import ru.netology.geo.GeoServiceImpl;
+import ru.netology.i18n.LocalizationService;
+import ru.netology.i18n.LocalizationServiceImpl;
 
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void messageSenderTest() {
+        GeoService geoService = Mockito.mock(GeoServiceImpl.class);
+//        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        Mockito.when(geoService.byIp(Mockito.<String>any()))
+                .thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
+        LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
+        Mockito.when(localizationService.locale(Country.RUSSIA))
+                .thenReturn("Это Россия !");
+
+    }
 }
